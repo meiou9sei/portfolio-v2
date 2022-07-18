3819a8c60c57886d8d911be3629c94ae76c7c0e8
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-07-18 18:23:55 -0400

    set up routers (commented out)

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Navbar /> */}
+        <div className="content">
+          <Home />
+          {/*
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/s/blog">
+              <Blog />
+            </Route>
+            <Route path="/s/resume">
+              <Resume />
+            </Route>
+            <Route path="/s/contact">
+              <Contact />
+            </Route>
+          </Switch>
+          */}
+        </div>
+      </div>
+    </Router>
   );
 }
 
