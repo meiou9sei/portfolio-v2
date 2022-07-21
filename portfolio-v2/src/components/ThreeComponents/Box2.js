4cf49fcbb6e984d01b21f34dcdf34cca8ed9f2eb
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-07-21 13:39:11 -0400

    figure out how to assign different colors to sheep

--- portfolio-v2/src/components/ThreeComponents/Box2.js
+++ portfolio-v2/src/components/ThreeComponents/Box2.js
@@ -1,6 +1,7 @@
 import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 
+
 const Box2 = () => {
     const mesh = useRef();
     useFrame((state, delta) => (mesh.current.rotation.x += 0.01, mesh.current.rotation.y += 0.01));
--- portfolio-v2/src/components/ThreeComponents/CanvasContainer.js
+++ portfolio-v2/src/components/ThreeComponents/CanvasContainer.js
@@ -1,13 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import Box from "./Box";
 import Box2 from "./Box2";
+import Sheep from "./Sheep";
+import CameraController from "./CameraController";
 
-import Sheep from './Sheep'
 
 const CanvasContainer = () => {
     return ( 
         <div id="canvas-container" style={{ height: '500px' }}>
             <Canvas>
+                {/* <CameraController /> */}
+
                 {/* lights */}
                 <ambientLight intensity={0.1} />
                 <directionalLight color="white" position={[-3, 10, 5]} />
