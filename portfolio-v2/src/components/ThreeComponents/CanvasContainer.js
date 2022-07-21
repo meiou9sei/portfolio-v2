db503c1c179de3e52eb87106a3382196fc0a4dde
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-07-21 12:40:50 -0400

    load sheep, simple rotation animation

--- portfolio-v2/src/components/ThreeComponents/CanvasContainer.js
+++ portfolio-v2/src/components/ThreeComponents/CanvasContainer.js
@@ -2,17 +2,21 @@ import { Canvas } from "@react-three/fiber";
 import Box from "./Box";
 import Box2 from "./Box2";
 
+import Sheep from './Sheep'
+
 const CanvasContainer = () => {
     return ( 
-        <div id="canvas-container">
+        <div id="canvas-container" style={{ height: '500px' }}>
             <Canvas>
                 {/* lights */}
                 <ambientLight intensity={0.1} />
-                <directionalLight color="red" position={[0, 0, 5]} />
+                <directionalLight color="white" position={[-3, 10, 5]} />
 
                 {/* objects */}
                 {/* <Box /> */}
-                <Box2 />
+                {/* <Box2 /> */}
+                <Sheep />
+                
             </Canvas>
         </div>
     );
