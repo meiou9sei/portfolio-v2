b1d44faec0262483e6847d19681466bcbffa7eff
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-07-26 15:24:40 -0400

    set up routers and links to work properly

--- portfolio-v2/src/components/Pages/ResumeComponents/ResumePage.js
+++ portfolio-v2/src/components/Pages/ResumeComponents/ResumePage.js
@@ -4,7 +4,7 @@ const ResumePage = () => {
             <div class="resume">
                 <h1>my resume</h1>
                 <p><a href="https://drive.google.com/file/d/16bu750wVymO4eRXSFWBcrDmzoWVNlt_U/view?usp=sharing">View my Resume on Google Drive</a></p>
-                <embed src="../Cameron-Avvampato-Resume.pdf" type="application/pdf" />
+                <embed src="../../../../public/Cameron-Avvampato-Resume.pdf" type="application/pdf" />
             </div>
         </main>
     );
