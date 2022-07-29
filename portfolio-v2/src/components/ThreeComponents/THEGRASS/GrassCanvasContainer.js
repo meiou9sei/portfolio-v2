49a73f40a181dc0815d27d7214ecbad7adbe505b
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-07-29 15:24:01 -0400

    god (random number) now assigns sheep animation as a prop

--- portfolio-v2/src/components/ThreeComponents/THEGRASS/GrassCanvasContainer.js
+++ portfolio-v2/src/components/ThreeComponents/THEGRASS/GrassCanvasContainer.js
@@ -4,21 +4,12 @@ import CameraController from "../CameraController";
 import { PerspectiveCamera } from "three";
 import RevivedAnimatedSheep from './../RevivedAnimatedSheep';
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); 
-}
-
-function getRandomNum(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
 //SHEEP SETTINGS
 const sheepScale = 3;
 //spawn position min and max x and z. also their movement range
 const spawnPosMinX = -50;
 const spawnPosMaxX = 50;
+const spawnPosY = -1;
 const spawnPosMinZ = 7; //change this according to camera setting so sheep don't clip top of Canvas
 const spawnPosMaxZ = -25;
 
@@ -32,6 +23,7 @@ for (let i = 0; i < getRandomInt(minSheepCount, maxSheepCount); i++) {
     SheepGroup.push(<RevivedAnimatedSheep 
         key={i} 
         position={ getValidSpawnCoords(0, spawnPosMinZ, spawnPosMinX, spawnPosMaxZ, spawnPosMaxX, spawnPosMaxZ) }
+        sheepAnimation={ getRandomSheepAnimation() }
         scale={sheepScale} 
         />)
 }
@@ -79,12 +71,26 @@ export default GrassCanvasContainer;
 
 
 
-/* THE SHEEP SPAWNER FORMULA */
+/*************/
+/* FUNCTIONS */
+/*************/
 
+/* RANDOM NUM FUNCTIONS */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+function getRandomNum(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/* THE SHEEP SPAWNER FORMULA */
 function getValidSpawnCoords(Ax, Ay, Bx, By, Cx, Cy) {
     
     let x; 
-    let y = 0; 
+    let y = spawnPosY; 
     let z;
 
     let validCoord = false;
@@ -96,7 +102,7 @@ function getValidSpawnCoords(Ax, Ay, Bx, By, Cx, Cy) {
         validCoord = checkIfCoordInTriangle(Ax, Ay, Bx, By, Cx, Cy, x, z);
     }
 
-    return [x, -1, z];
+    return [x, y, z];
 }
 
 /* based on this math https://www.youtube.com/watch?v=HYAgJN3x4GA, 
@@ -111,3 +117,21 @@ function checkIfCoordInTriangle(Ax, Ay, Bx, By, Cx, Cy, Px, Py) {
         return false;
     }
 }
+
+/* SET SHEEP ANIMATION RANDOMLY */
+function getRandomSheepAnimation() {
+    //currently only 2 sheep animation modes - eating or walking
+    let godsWill = getRandomNum(0, 1);
+    let godsCommand = "TPose";
+
+    if (godsWill < 0.5) {
+        console.log("YOU SHALL BE AN EATER");
+        godsCommand = "Eater";
+    } else {
+        console.log("YOU SHALL BE A WALKER");
+        godsCommand = "Walker";
+    }
+
+    return godsCommand;
+}
+
