0bd003fc5482a843c87c41cc5ccde886a91abdad
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-07-29 22:09:34 -0400

    smoothscroll now works

--- portfolio-v2/src/components/Pages/HomeComponents/Hero.js
+++ portfolio-v2/src/components/Pages/HomeComponents/Hero.js
@@ -1,10 +1,3 @@
-document.querySelectorAll(".about-nav").forEach(e => {e.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-    });
-})});
-
 const Hero = () => {
     return (
         <div>
