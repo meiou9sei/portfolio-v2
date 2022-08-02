10aed51b5d3d5aa2ee149c9c451913192c5e84f2
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-01 22:03:40 -0400

    adjust some skill descriptions

--- portfolio-v2/src/components/Pages/HomeComponents/aboutMeMinis.js
+++ portfolio-v2/src/components/Pages/HomeComponents/aboutMeMinis.js
@@ -1,11 +1,11 @@
 const aboutMeMinis = {
     mini1: { category: 'What I do 🔨', title: null, subtitle: null, skills: ['Creative Tech', 'Frontend Web Development', 'Touch Grass'] },
-    mini2: { category: 'Skills 🔧', title: 'Tech 🧪', subtitle: null, skills: ['HTML/CSS/Javascript', 'React', 'BrightAuthor (BrightScreen)'] },
+    mini2: { category: 'Skills 🔧', title: 'Tech 🧪', subtitle: null, skills: ['HTML/CSS/Javascript', 'React', 'Three.js with React Three Fiber', 'BrightAuthor (BrightScreen)'] },
     mini3: { category: 'Skills 🔧', title: 'Graphics 🖌️', subtitle: null, skills: ['Video Production: Premiere Pro, AfterEffects', 'Graphics: Photoshop, PaintTool SAI 2', 'Animation: Blender'] },
-    mini4: { category: 'Skills 🔧', title: 'Languages 🤟', subtitle: null, skills: ['Native: English, Japanese', 'Fluent: German (B2)', 'Learning: American Sign Language (A2), Korean (A1)'] },
-    mini5: { category: 'Experience ⚙️', title: 'Creative Tech Intern, Elephant / MAIP 2022 🐘', subtitle: '2022-current', skills: ['Websites and Interactive Game Apps using React', 'Creating BrightScreen presentations with Bright Author', 'Working with multiple teams and projects simultaneously'] },
-    mini6: { category: 'Experience ⚙️', title: 'Political Science Major, UCF 🦄', subtitle: '2018-2022', skills: ['Learning Politics and History', 'CS & IT classes', 'Self-teaching webdev :p'] },
-    mini7: { category: 'Fun Facts 🍜', title: null, subtitle: 'Wow, uhm... you came all the way down here? Have some fun facts', skills: ['Full Name: Cameron Naoki Matsuda Avvampato', 'LoL: Hardstuck D4 Annie onetrick', 'Bouldering: Can climb V4s and V5s if you give me an hour', 'Alive: Probably'] },
+    mini4: { category: 'Skills 🔧', title: 'Languages 🤟', subtitle: null, skills: ['Native: English, Japanese', 'Fluent: German (B2)', 'Learning: Spanish (A1), Korean (A1)'] },
+    mini5: { category: 'Experience ⚙️', title: 'Creative Tech Intern, Elephant / MAIP 2022 🐘', subtitle: '2022-current', skills: ['Websites and Interactive Game Apps using React', 'Creating BrightScreen presentations with BrightAuthor', 'Learning React, Three JS, and React Three Fiber'] },
+    mini6: { category: 'Experience ⚙️', title: 'Political Science Major, UCF 🦄', subtitle: '2018-2022', skills: ['Learning Politics and History', 'CS & IT classes', 'Self-taught webdev :p', 'CS50 and The Odin Project'] },
+    mini7: { category: 'Fun Facts 🍜', title: null, subtitle: 'Wow, uhm... you came all the way down here? Have some fun facts', skills: ['Only orders the Wisconsin Mac & Cheese with 10 Korean BBQ Meatballs 8 years now', 'LoL: Hardstuck D4 Annie onetrick', 'Bouldering: Can climb V4s and V5s if you give me an hour', 'Alive: Probably'] },
 };
 
 export default aboutMeMinis;
