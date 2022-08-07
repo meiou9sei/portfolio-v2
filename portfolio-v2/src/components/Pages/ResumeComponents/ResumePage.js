e1375606994ccb2d89478e914e6fe95913030db2
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-07 01:07:47 -0400

    update resume, aboutme content

--- portfolio-v2/src/components/Pages/ResumeComponents/ResumePage.js
+++ portfolio-v2/src/components/Pages/ResumeComponents/ResumePage.js
@@ -3,7 +3,7 @@ const ResumePage = () => {
         <main className="subpage-main resumepage">
             <div className="resume">
                 <h1>my resume</h1>
-                <p><a href="https://drive.google.com/file/d/16bu750wVymO4eRXSFWBcrDmzoWVNlt_U/view?usp=sharing">View my Resume on Google Drive</a></p>
+                <p><a href="https://drive.google.com/file/d/18Dg3shJ4OLRc2IR0W-RT87p9h9nDUhti/view?usp=sharing">View my Resume on Google Drive</a></p>
                 <embed src={process.env.PUBLIC_URL + "/Cameron-Avvampato-Resume.pdf"} type="application/pdf" />
             </div>
         </main>
